3e1e1ae03075a4df680c93651259ccb5cfe7c93b
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-12-21 15:51:31 -0300

    when pass description to a image, render it

--- components/image/Image.js
+++ components/image/Image.js
@@ -1,3 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Image = () => <div data-testid="image">hi!</div>
+export const Image = ({ description }) => <div data-testid="image">
+    {description && <div className="description">
+        {description}
+    </div>}
+</div>
+
+Image.propTypes = {
+    description: PropTypes.string
+}
--- components/image/Image.spec.js
+++ components/image/Image.spec.js
@@ -2,9 +2,21 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Image } from './Image'
 
+const getDescription = (component) => component.find('.description')
+
 describe('Image', () => {
-    it('smoke test', () => {
+    describe('when pass a description', () => {
+        const aDescription = "description !"
+        const component = shallow(<Image description={aDescription} />)
+        it('render it', () => {
+            expect(getDescription(component).text()).toEqual(aDescription)
+        })
+    })
+
+    describe('when NO pass a description', () => {
         const component = shallow(<Image />)
-        expect(component.exists()).toBe(true)
+        it('dont render it', () => {
+            expect(getDescription(component).exists()).toBe(false)
+        })
     })
 })
