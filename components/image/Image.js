d19514e130c184de2505619d8004a23d958356df
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-12-21 15:59:59 -0300

    when pass a src  to  Image component, render it

--- components/image/Image.js
+++ components/image/Image.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const Image = ({ description }) => <div data-testid="image">
+export const Image = ({ description, src }) => <div data-testid="image">
+    {
+        src
+            ? <img className="img-gallery" src={src} />
+            : <span className="no-img">This image is not available.</span>
+    }
     {description && <div className="description">
         {description}
     </div>}
 </div>
 
 Image.propTypes = {
-    description: PropTypes.string
+    description: PropTypes.string,
+    src: PropTypes.string
 }
--- components/image/Image.spec.js
+++ components/image/Image.spec.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme'
 import { Image } from './Image'
 
 const getDescription = (component) => component.find('.description')
+const getImg = (component) => component.find('.img-gallery')
+const getNoImage = (component) => component.find('.no-img')
 
 describe('Image', () => {
     describe('when pass a description', () => {
@@ -17,6 +19,21 @@ describe('Image', () => {
         const component = shallow(<Image />)
         it('dont render it', () => {
             expect(getDescription(component).exists()).toBe(false)
+        })
+    })
+
+    describe('when pass a src', () => {
+        const aSrc = "description !"
+        const component = shallow(<Image src={aSrc} />)
+        it('render it', () => {
+            expect(getImg(component).props().src).toEqual(aSrc)
+        })
+    })
+
+    describe('when NO pass a src', () => {
+        const component = shallow(<Image />)
+        it('put a empty message', () => {
+            expect(getNoImage(component).text()).toEqual("This image is not available.")
         })
     })
 })
